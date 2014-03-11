99428eb97212c32fdcc3c84d06bb24f29b4b76e7
Tomas Francisco <mail@tomasfrancisco.com>
2014-03-11 23:38:15 +0000

    bar_challange_create_form_views

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,11 +1,20 @@
 class BarsController < ApplicationController
-def index
-  	@bar = Bar.find(:all)
-  end
-  def new
-  	@bar = Bar.new
-  end
-  def create
-  	@bar = Bar.new(params[:bar])
-  end
+
+	def index
+  		@bar = Bar.find(:all)
+  	end
+  	def new
+  		@bar = Bar.new
+  	end
+  	def create
+  		@bar = Bar.create(bar_params)
+  	end
+
+  	private
+
+  	def bar_params
+  		allow = [:name, :description, :lat, :long, :type, :email]
+  		params.require(:bar).permit(allow)
+  	end
+
 end
--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,18 @@
 class ChallengesController < ApplicationController
   def index
-  end
+  		@challenge = Challenge.find(:all)
+  	end
+  	def new
+  		@challenge = Challenge.new
+  	end
+  	def create
+  		@challenge = Challenge.create(challenge_params)
+  	end
+
+  	private
+
+  	def challenge_params
+  		allow = [:name, :description, :points]
+  		params.require(:challenge).permit(allow)
+  	end
 end
